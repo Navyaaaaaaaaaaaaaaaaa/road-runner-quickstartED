728e13aa73e979d4fe523bdf2bdd775b472b61b2
SaanviMs <saanvi.madagani@gmail.com>
2025-11-22 14:12:48 -0800

    Auto Testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -15,7 +16,41 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-57, 43, Math.toRadians(-60)))
-                .turn(Math.toRadians(90))
+                //afterTime(Math.max(0, EST_TRAVEL_SEC-2.5), startShooter())
+                        .strafeToLinearHeading(new Vector2d(-40, -25), Math.toRadians(-130))
+                       // .afterTime(1,startIntake())
+                        .waitSeconds(3)
+                        //.afterTime(1.5,stopShooter())
+                        .strafeToLinearHeading(new Vector2d(-9, -31), Math.toRadians(-90))
+                       // .afterTime(0,startIntake())
+                        .strafeTo(new Vector2d(-9, -39))
+                        .strafeTo(new Vector2d(-9, -47))
+                       // .afterTime(0,stopIntake())
+                       // .afterTime(Math.max(0, EST_TRAVEL_SEC-2.5), startShooter())
+                        .strafeToLinearHeading(new Vector2d(-40, -25), Math.toRadians(-130))
+                       // .afterTime(1,startIntake())
+                        .waitSeconds(3)
+                        //.afterTime(1.5,stopShooter())
+                       // .afterTime(0,startIntake())
+                        .strafeToLinearHeading(new Vector2d(12, -31), Math.toRadians(-90))
+                        .strafeTo(new Vector2d(12, -39))
+                        .strafeTo(new Vector2d(12, -47))
+                       // .afterTime(0,startIntake())
+                       // .afterTime(Math.max(0, EST_TRAVEL_SEC-2.5), startShooter())
+                        .strafeToLinearHeading(new Vector2d(-40, -25), Math.toRadians(-130))
+                       // .afterTime(1,startIntake())
+                        .waitSeconds(3)
+                       // .afterTime(1.5,stopShooter())
+                       // .afterTime(0,startIntake())
+                        .strafeToLinearHeading(new Vector2d(35, -31), Math.toRadians(-90))
+                        .strafeTo(new Vector2d(35, -39))
+                        .strafeTo(new Vector2d(35, -48))
+                       // .afterTime(0,stopIntake())
+                       // .afterTime(Math.max(0, EST_TRAVEL_SEC-2.5), startShooter())
+                        .strafeToLinearHeading(new Vector2d(-40, -25), Math.toRadians(-130))
+                       // .afterTime(1,startIntake())
+                        .waitSeconds(3)
+                       // .afterTime(1.5,stopShooter())
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_BLACK)
